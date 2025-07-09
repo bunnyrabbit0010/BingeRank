1b4a043c56248e698469ee6f360c2e4cb13ac8e8
“Bunny <BunnyRabbit_aws@gmail.com>
2025-07-08 21:36:33 -0700

    added endpoint for hello

--- binge_rank_backend/app/main.py
+++ binge_rank_backend/app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "BingeRank API is up and running!"}
+
+@app.get("/hello")
+def say_hello():
+    return {"message": "Hello, World!"}

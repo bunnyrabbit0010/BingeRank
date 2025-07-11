a78c3935127671a4f671a3f25e5ff8d0cf076b58
“Bunny <BunnyRabbit_aws@gmail.com>
2025-07-10 23:07:42 -0700

    removed unneccessary comments and unused imports

--- binge_rank_backend/app/main.py
+++ binge_rank_backend/app/main.py
@@ -40,28 +40,6 @@ def read_root():
     return {"message": "BingeRank API is up and running!"}
 
 
-# Parse the URL
-url = "postgresql://postgres.tfsagqekvebxrbjxxdhe:FSacOoliZZbDqgYv@aws-0-us-east-2.pooler.supabase.com:6543/postgres"
-result = urlparse(url)
-@app.get("/checkdb")
-def check_db():
-    print("Checking database connection...")
-
-    conn = pg8000.native.Connection(
-        user=result.username,
-        password=result.password,
-        host=result.hostname,
-        port=result.port,
-        database=result.path.lstrip("/"),
-        ssl_context = ssl._create_unverified_context()
-    )
-
-    print("✅ Connected. Server time:", conn.run("SELECT current_timestamp;"))
-    conn.close()
-    print("✅ Connection successful")
-
-
-
 @app.get("/users")
 async def get_users():
     query = users.select()
